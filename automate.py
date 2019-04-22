c171e49dec605b3835f0b9d0bf53ae1edec3e840
Asutosh <asupat12@gmail.com>
2019-04-21 21:31:01 -0400

    Refactor

--- automate.py
+++ automate.py
@@ -1,41 +1,29 @@
 from nipype.interfaces import fsl
+from cleaning import cleanUp
 
 import os
 
+def fsl_run(images_path,output_path):
+    first = fsl.FIRST()
+    file_Names = os.listdir(images_path)
+    images_path = path = "/home/nikhil/Spine/Pipeline/MRI-Output"
 
-#MriFile = ["sample-001.nii.gz","sample-002.nii.gz","sample-003.nii.gz","sample-004.nii.gz"]
-
-first = fsl.FIRST()
-
-path = "/home/nikhil/Spine/Pipeline/MRI-Output"
-
-try:  
-    os.mkdir(path)
-except OSError:  
-    print ("Creation of the directory %s failed" % path)
-    os.chdir(path)
-else:  
-    print ("Successfully created the directory %s " % path)
-
-print(os.getcwd())
-first.inputs.in_file = 'sample-001.nii.gz'
-first.inputs.out_file = 'output.nii'
-print("Working - 1")
-try:
-    res = first.run()
-except Exception as e:
-    print(e)
-
-
-
-"""
-for i in range(len(MriFile)):
-    first.inputs.in_file = MriFile[i]
-    first.inputs.out_file = 'output.nii'
-    print("Working - ",i)
     try:
-       res = first.run()
-    except Exception as e:
-       print(e)
+        os.mkdir(path)
+    except OSError:
+        #print("Creation of the directory %s failed" % path)
+        os.chdir(path)
+    else:
+        print("Successfully created the directory %s " % path)
 
-"""
+    print(os.getcwd())
+    first.inputs.in_file = 'sample-001.nii.gz'
+    first.inputs.out_file = 'output.nii'
+    print("Working - 1")
+    try:
+        res = first.run()
+    except Exception as e:
+        print(e)
+
+path = os.getcwd()
+fsl_run(path)
