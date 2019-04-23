c73411d7ea38488aab4532a2e6808d126955ee30
nikjz <nikhil.flash9876@gmail.com>
2019-04-22 17:16:01 -0700

    Add support for multiple files

--- automate.py
+++ automate.py
@@ -1,32 +1,32 @@
 from nipype.interfaces import fsl
 from cleaning import cleanUp
+import shutil
 
 import os
 
 def fsl_run(images_path,output_path):
     first = fsl.FIRST()
     file_Names = os.listdir(images_path)
-    images_path = path = "/home/nikhil/Spine/Pipeline/MRI-Output"
-
-    try:
-        os.mkdir(path)
-    except OSError:
-        #print("Creation of the directory %s failed" % path)
-        os.chdir(path)
-    else:
-        print("Successfully created the directory %s " % path)
 
     #iterating over given path
     for i in range(len(file_Names)):
-        first.inputs.in_file = file_Names[i]
-        output_file_name = i+'output.nii'
+    	shutil.copy(images_path+"/"+file_Names[i], output_path)
+        first.inputs.in_file = output_path+"/"+file_Names[i]
+        output_file_name = str(i)+'output.nii'
         first.inputs.out_file = output_path+"/"+output_file_name
-
+        output_log = str(i)+'output'
         try:
-            res = first.run()
-            cleanUp(output_path,output_file_name)
+        	print("Run - 1")
+        	res = first.run()
+        	print("output - 1")
+        	cleanUp(output_path,output_log)
         except Exception as e:
-            print(e)
+            print(e,"------tesing")
+
+        cleanUp(output_path,output_log)
+        print("output - ",i)
 
 path = os.getcwd()
-fsl_run(path)
+images_path = str(path)+'/MRI-Input'
+output_path = str(path)+'/MRI-Output'
+fsl_run(images_path,output_path)
--- cleaning.py
+++ cleaning.py
@@ -7,12 +7,16 @@ import shutil
 
 def cleanUp(output_path, output_file):
 
-	output_path = cwd = '/home/nikhil/Spine/Try1'
-
 	# Delete Dir
-	shutil.rmtree(output_file + ".logs")
-
+	shutil.rmtree(output_path+"/"+output_file + ".logs")
+	print("CleanUp Called")
 	for CleanUp in glob.glob(output_path+'/*'):
 		if not CleanUp.endswith('_all_fast_firstseg.nii.gz'):
 			print("Delete")
 			os.remove(CleanUp)
+"""
+path = os.getcwd()
+output_path = str(path)+"/MRI-Output"
+output_file = '0output'
+cleanUp(output_path, output_file)
+"""
