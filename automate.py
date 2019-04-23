89d080b29f03fe27b2cca3fbb4d56d5561d0eadf
Asutosh <asupat12@gmail.com>
2019-04-23 01:33:33 -0400

    Integrate fsl with pyrdiomics

--- automate.py
+++ automate.py
@@ -1,32 +1,40 @@
 from nipype.interfaces import fsl
 from cleaning import cleanUp
+from execute import volume_calc
 import shutil
-
+import sys
 import os
 
 def fsl_run(images_path,output_path):
     first = fsl.FIRST()
-    file_Names = os.listdir(images_path)
+    file_names = os.listdir(images_path)
 
     #iterating over given path
-    for i in range(len(file_Names)):
-    	shutil.copy(images_path+"/"+file_Names[i], output_path)
-        first.inputs.in_file = output_path+"/"+file_Names[i]
-        output_file_name = str(i)+'output.nii'
-        first.inputs.out_file = output_path+"/"+output_file_name
-        output_log = str(i)+'output'
+    for image in range(len(file_names)):
+        shutil.copy(images_path + "/" + file_names[image], output_path)
+        first.inputs.in_file = output_path + "/" + file_names[image]
+        output_file_name = str(file_names[image]) + 'output.nii'
+        first.inputs.out_file = output_path + "/" + output_file_name
+        output_log = str(file_names[image])+'output'
         try:
-        	print("Run - 1")
-        	res = first.run()
-        	print("output - 1")
-        	cleanUp(output_path,output_log)
+            print("Running " + file_names[image])
+            res = first.run()
         except Exception as e:
-            print(e,"------tesing")
+            print("Error running - " + file_names[image])
 
+        print("Cleaning up unnecessary files of - " + file_names[image])
         cleanUp(output_path,output_log)
-        print("output - ",i)
+        print("Calculating Volumed for each region of - " + output_file_name)
+        volume_calc(output_path + "/" + output_file_name)
 
-path = os.getcwd()
-images_path = str(path)+'/MRI-Input'
-output_path = str(path)+'/MRI-Output'
-fsl_run(images_path,output_path)
+if __name__ == '__main__':
+    path = sys.argv[0]
+
+    if(os.path.isdir("/MRI-Output") == False):
+        os.mkdir("MRI-Output")
+    if (os.path.isdir("/MRI-Input") == False):
+        os.mkdir("MRI-Intput")
+
+    images_path = str(path)+'/MRI-Input'
+    output_path = str(path)+'/MRI-Output'
+    fsl_run(images_path, output_path)
--- cleaning.py
+++ cleaning.py
@@ -8,15 +8,7 @@ import shutil
 def cleanUp(output_path, output_file):
 
 	# Delete Dir
-	shutil.rmtree(output_path+"/"+output_file + ".logs")
-	print("CleanUp Called")
+	shutil.rmtree(output_path + "/" + output_file + ".logs")
 	for CleanUp in glob.glob(output_path+'/*'):
 		if not CleanUp.endswith('_all_fast_firstseg.nii.gz'):
-			print("Delete")
 			os.remove(CleanUp)
-"""
-path = os.getcwd()
-output_path = str(path)+"/MRI-Output"
-output_file = '0output'
-cleanUp(output_path, output_file)
-"""
